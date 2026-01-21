9cc391b6e90314156de94144f45677e357e1dcef
Nick Garland <ngarland@academic.rrc.ca>
2026-01-21 16:51:10 -0600

    Added: API endpoint fot the calculate portfolio function

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Express } from "express";
+import { calculatePortfolioPerformance } from "./portfolio/portfolioPerformance";
 
 // Initialize Express application
 const app: Express = express();
@@ -10,6 +11,7 @@ interface HealthCheckResponse {
     version: string;
 }
 
+// Returns the health of the server
 app.get("/api/v1/health", (req, res) => {
     const healthData: HealthCheckResponse = {
         status: "OK",
@@ -18,6 +20,14 @@ app.get("/api/v1/health", (req, res) => {
         version: "1.0.0",
     };
     res.json(healthData);
+});
+
+// Returns the calculated portfolio
+app.get("/api/v1/portfolio/performance", (req, res) => {
+    const initialInvestment = Number(req.query.initialInvestment);
+    const currentValue = Number(req.query.currentValue);
+
+    res.json(calculatePortfolioPerformance(initialInvestment, currentValue))
 });
 
 export default app;
